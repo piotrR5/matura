2d622ab1e564a02332c1a3a114541df729108609
Piotr Rodkiewicz <prodkiewicz.w@gmail.com>
2023-05-03 14:52:23 +0200

    4

---
+++ cpp/findSubString.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+/*
+#include <string>
+#include <iostream>
+#Include <algorithm>
+*/
+
+using namespace std;
+
+int findSubString(string s, string sub){
+    if(s.find(sub)!=string::npos)return s.find(sub);
+    else return -1;
+}
+
+int main(){
+    string s="sgjdklgjlkgjasjagjsdg;sdgfoxerlgdksdkgl;dgldg";
+    string sub="foxer";
+    cout<<findSubString(s,sub)<<endl;
+}
