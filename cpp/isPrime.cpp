83e9e0287d5cda03ec29bf1e65ce9e399eda9453
Koobson <99011139+Koobson@users.noreply.github.com>
2023-05-03 14:32:24 +0200

    Add files via upload

---
+++ cpp/isPrime.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+//#include<iostream>
+
+using namespace std;
+
+bool isPrime(int num){
+    if(num==1) return false;
+    if(num==2) return true;
+    for(int i=2; i*i<=num; i++){
+        if(num%i==0) return false;
+    }
+    return true;
+}
+
+int main(){
+    cout<<isPrime(176);
+}
